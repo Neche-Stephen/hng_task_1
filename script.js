dc23eef98e3ff55c3cdf52fe5b1ee1587d679498
Chinecherem <chinecheremubawike@gmail.com>
2023-09-09 07:37:11 +0100

    Completed task 1 of hng

---
+++ script.js
@@ -0,0 +1,10 @@
+const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+const currentUTCTime = new Date().getTime();
+currentUTCTimeElement.textContent = `Current UTC Time: ${currentUTCTime} milliseconds`;
+
+let dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let currentDate = new Date();
+let dayOfWeek = currentDate.getDay();
+let currentDayOfWeek = daysOfWeek[dayOfWeek];
+dayOfWeekElement.textContent = currentDayOfWeek;
